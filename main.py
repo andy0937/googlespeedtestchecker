d27394b835fcc5e38884d058732dd9e346a0c828
Arsen <daa_91@mail.ru>
2016-10-05 21:01:17 +0300

    make some beauty
    changed csv divider, removed some comments from main.py

--- main.py
+++ main.py
@@ -12,28 +12,27 @@ def makeRequest(url, strategy, api_key ):
     r = requests.get(link)
     return r.json()
 
-def parseJSON(responce):
-    id = responce['id']
-    r = responce['ruleGroups']
+
+def parseJSON(response):
+    id = response['id']
+    r = response['ruleGroups']
     r1 = r['SPEED']
     r.clear()
     score = r1['score']
     r1.clear()
     return {
-        'id': id,
-        'score': str(score)
-    }
+            'id': id,
+            'score': str(score)}
 
 
 if __name__ == '__main__':
     currentURL = open(config.sitemap)
     output = open(config.outputlog, 'w')
-    #i=0
     for line in tqdm(currentURL.readlines()):
         k = makeURL(line.strip('\t\n'))
         req = makeRequest(k, config.strategy, config.api_key)
         vals = parseJSON(req)
-        writeStr = vals['id'] + ',' + vals['score'] + '\n'
+        writeStr = vals['id'] + '|' + vals['score'] + '\n'
         output.write(writeStr)
 currentURL.close()
 output.close()
