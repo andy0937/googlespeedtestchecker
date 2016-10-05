5742987236e8c891ec23d28a53ef4eca075a4aa6
Arsen <daa_91@mail.ru>
2016-10-05 10:51:03 +0300

    0.1 release

---
+++ config.py
@@ -0,0 +1,6 @@
+api_key = 'key' #google api key here
+host = 'http://foo.bar' #host
+strategy = "mobile"  #must be "desktop" or "mobile", returns result for selected variant
+pagespeedHost = "https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=" #speedtest host
+sitemap = 'sitemap.txt' #sitemap filename
+outputlog = 'result_'+strategy+'.csv'  #results file
---
+++ main.py
@@ -0,0 +1,39 @@
+import requests
+import config
+from tqdm import tqdm
+
+
+def makeURL(link):
+    URL = config.host+link
+    return URL
+
+def makeRequest(url, strategy, api_key ):
+    link = config.pagespeedHost + url+"&strategy="+strategy+"&fields=id%2CruleGroups"+"&key="+api_key
+    r = requests.get(link)
+    return r.json()
+
+def parseJSON(responce):
+    id = responce['id']
+    r = responce['ruleGroups']
+    r1 = r['SPEED']
+    r.clear()
+    score = r1['score']
+    r1.clear()
+    return {
+        'id': id,
+        'score': str(score)
+    }
+
+
+if __name__ == '__main__':
+    currentURL = open(config.sitemap)
+    output = open(config.outputlog, 'w')
+    #i=0
+    for line in tqdm(currentURL.readlines()):
+        k = makeURL(line.strip('\t\n'))
+        req = makeRequest(k, config.strategy, config.api_key)
+        vals = parseJSON(req)
+        writeStr = vals['id'] + ',' + vals['score'] + '\n'
+        output.write(writeStr)
+currentURL.close()
+output.close()
